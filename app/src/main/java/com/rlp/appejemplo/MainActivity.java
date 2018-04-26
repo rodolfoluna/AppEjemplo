4134cfc1c8701bc2a3f474e32daac2c91f9c029c
Rodolfo Luna <rodolfo.lunap@gmail.com>
2018-04-26 15:55:28 -0500

    Splash screen y Home

--- app/src/main/java/com/rlp/appejemplo/MainActivity.java
+++ app/src/main/java/com/rlp/appejemplo/MainActivity.java
@@ -1,13 +1,25 @@
 package com.rlp.appejemplo;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int DELAY_TIME=3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Handler handler= new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },DELAY_TIME);
     }
 }
